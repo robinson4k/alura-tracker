f78efb98e94a67b68a5bf5c29db31d1e6b9b70e1
Robinson Silva <robinson.silva@piauiconectado.com.br>
2022-05-17 07:58:44 -0400

    Primeiro commit

---
+++ src/components/BarraLateral.vue
@@ -0,0 +1,29 @@
+<template>
+    <header>
+        <h1>
+            <img src="../assets/logo.png" alt="">
+        </h1>
+    </header>
+</template>
+
+<script lang="ts">
+    import { defineComponent } from "vue";
+    export default defineComponent({
+        name: 'BarraLateral',
+    })
+</script>
+
+<style scoped>
+    header {
+        padding: 1rem;
+        width: 100%;
+        height: 100vh;
+        background: #0d3d66;
+    }
+    @media only screen and (max-width: 768px) {
+        header {
+            padding: 2.5rem;
+            height: auto;
+        }
+    }
+</style>
---
+++ src/components/Formulario.vue
@@ -0,0 +1,44 @@
+<template>
+  <div class="box">
+    <div class="columns">
+      <div
+        class="column is-8"
+        role="form"
+        aria-label="Formulário para criação de uma nova tarefa"
+      >
+        <input type="text" class="input" placeholder="Qual tarefa você deseja iniciar?" />
+      </div>
+      <div class="column">
+        <div class="is-flex is-align-items-center is-justify-content-space-between">
+          <section>
+            <strong>00:00:00</strong>
+          </section>
+          <button class="button">
+              <span class="icon">
+                  <i class="fas fa-play"></i>
+              </span>
+              <span>play</span>
+          </button>
+          <button class="button">
+              <span class="icon">
+                  <i class="fas fa-stop"></i>
+              </span>
+              <span>stop</span>
+          </button>
+        </div>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script lang="ts">
+
+  import { defineComponent } from "vue";
+
+  export default defineComponent({
+    name: "Formulario",
+  });
+</script>
+
+<style scoped>
+</style>
