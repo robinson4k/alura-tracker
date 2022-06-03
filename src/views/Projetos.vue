6e97c82b680e8ccc552294d479f78f425fa61c7e
Robinson Silva <robinson.silva@piauiconectado.com.br>
2022-06-03 18:00:56 -0400

    Cadastrando projetos

--- src/views/Projetos.vue
+++ src/views/Projetos.vue
@@ -1,8 +1,38 @@
 <template>
-    <h1>Projetos</h1>
+    <section class="projetos p-3">
+        <h1 class="title">Projetos</h1>
+        <form @submit.prevent="salvar">
+            <div class="field">
+                <label for="nomeDoProjeto" class="label">Nome do projeto</label>
+                <input type="text" class="input" v-model="nomeDoProjeto" id="nomeDoProjeto">
+            </div>
+
+            <div class="field">
+                <button type="submit" class="button">Salvar</button>
+            </div>
+        </form>
+
+        <table class="table is-fullwidth">
+            <thead>
+                <tr>
+                    <th>ID</th>
+                    <th>Nome</th>
+                    <th></th>
+                </tr>
+            </thead>
+            <tbody>
+                <tr v-for="projeto in projetos" :key="projeto.id">
+                    <td>{{ projeto.id }}</td>
+                    <td>{{ projeto.nome }}</td>
+                    <td></td>
+                </tr>
+            </tbody>
+        </table>
+    </section>
 </template>
 
 <script lang="ts">
+import IProjeto from '@/interfaces/IProjeto';
 import { defineComponent } from 'vue';
 
 export default defineComponent({
@@ -11,11 +41,21 @@ export default defineComponent({
     },
     data() {
         return {
+            nomeDoProjeto: '',
+            projetos: [] as IProjeto[]
         }
     },
     computed: {
     },
     methods: {
+        salvar() {
+            const projeto: IProjeto = {
+                nome: this.nomeDoProjeto,
+                id: new Date().toISOString()
+            }
+            this.projetos.push(projeto)
+            this.nomeDoProjeto = ''
+        }
     }
 });
 </script>
