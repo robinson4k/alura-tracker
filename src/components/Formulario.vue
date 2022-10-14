38bfef098813108b15a7e9c41413180659cf662e
Robinson Silva <robinson.silva@piauiconectado.com.br>
2022-10-13 20:25:13 -0400

    03 Modificando o estado

--- src/components/Formulario.vue
+++ src/components/Formulario.vue
@@ -2,7 +2,7 @@
   <div class="box formulario">
     <div class="columns">
       <div
-        class="column is-8"
+        class="column is-5"
         role="form"
         aria-label="Formulário para criação de uma nova tarefa"
       >
@@ -13,6 +13,20 @@
           v-model="descricao"
         />
       </div>
+      <div class="column is-3">
+        <div class="select">
+          <select v-model="idProjeto">
+            <option value="">Selecione o projeto</option>
+            <option
+              :value="projeto.id"
+              v-for="projeto in projetos"
+              :key="projeto.id"
+            >
+              {{ projeto.nome }}
+            </option>
+          </select>
+        </div>
+      </div>
       <div class="column">
         <Temporizador @aoTemporizadorFinalizado="finalizarTarefa"/>
       </div>
@@ -21,8 +35,10 @@
 </template>
 
 <script lang="ts">
-  import { defineComponent } from "vue";
+  import { computed, defineComponent } from "vue";
+  import { useStore } from "vuex";
   import Temporizador from './Temporizador.vue'
+  import {key} from '@/store'
 
   export default defineComponent({
     name: "Formulario",
@@ -32,7 +48,8 @@
     },
     data() {
       return {
-        descricao: ''
+        descricao: '',
+        idProjeto: '',
       }
     },
     methods: {
@@ -40,8 +57,15 @@
         this.$emit('aoSalvarTarefa', {
           duracaoEmSegundos: tempoDecorrido,
           descricao: this.descricao,
+          projeto: this.projetos.find(proj => proj.id == this.idProjeto)
         })
         this.descricao = ''
+      }
+    },
+    setup() {
+      const store = useStore(key)
+      return {
+        projetos: computed(() => store.state.projetos)
       }
     }
   });
