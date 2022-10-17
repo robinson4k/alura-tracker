dc20069aa8cd2546199e7d914581f92d27b84c85
Robinson Silva <robinson.silva@piauiconectado.com.br>
2022-10-16 20:19:23 -0400

    04 Setup da store

--- src/store/index.ts
+++ src/store/index.ts
@@ -1,5 +1,5 @@
 import IProjeto from '@/interfaces/IProjeto'
-import {createStore, Store} from 'vuex'
+import {createStore, Store, useStore as vuexUseStore} from 'vuex'
 import {InjectionKey} from 'vue'
 
 interface Estado {
@@ -10,19 +10,19 @@ export const key: InjectionKey<Store<Estado>> = Symbol()
 
 export const store = createStore<Estado>({
     state: {
-        projetos: [
-            {
+        projetos: []
+    },
+    mutations: {
+        'ADICIONA_PROJETO'(state, nomeDoProjeto: string) {
+            const projeto = {
                 id: new Date().toISOString(),
-                nome: 'TypeScript',
-            },
-            {
-                id: new Date().toISOString(),
-                nome: 'Vue',
-            },
-            {
-                id: new Date().toISOString(),
-                nome: 'Vuex',
-            }
-        ]
+                nome: nomeDoProjeto
+            } as IProjeto
+            state.projetos.push(projeto)
+        }
     }
 })
+
+export function useStore(): Store<Estado> {
+    return vuexUseStore(key)
+}
--- src/views/Projetos.vue
+++ src/views/Projetos.vue
@@ -38,7 +38,8 @@
 
 <script lang="ts">
 import IProjeto from '@/interfaces/IProjeto';
-import { defineComponent } from 'vue';
+import { useStore } from '@/store';
+import { computed, defineComponent } from 'vue';
 
 export default defineComponent({
     name: "Projetos",
@@ -46,20 +47,20 @@ export default defineComponent({
     },
     data() {
         return {
-            nomeDoProjeto: '',
-            projetos: [] as IProjeto[]
+            nomeDoProjeto: ''
         }
-    },
-    computed: {
     },
     methods: {
         salvar() {
-            const projeto: IProjeto = {
-                nome: this.nomeDoProjeto,
-                id: new Date().toISOString()
-            }
-            this.projetos.push(projeto)
+            this.store.commit('ADICIONA_PROJETO', this.nomeDoProjeto)
             this.nomeDoProjeto = ''
+        }
+    },
+    setup () {
+        const store = useStore()
+        return {
+            store,
+            projetos: computed(() => store.state.projetos)
         }
     }
 });
