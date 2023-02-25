52303acfffe771744249c4cbebd94dedc370f744
Robinson Silva <robinson.silva@piauiconectado.com.br>
2023-02-25 18:46:04 -0400

    Criando o componente de notificação

---
+++ src/components/Notificacoes.vue
@@ -0,0 +1,27 @@
+<template>
+    <div class="notificacoes">
+        <article class="message is-success">
+            <div class="message-header">Atenção!</div>
+            <div class="message-body">
+                Aqui vai um texto 
+            </div>
+        </article>
+    </div>
+</template>
+
+<script lang="ts">
+    import { defineComponent } from "vue";
+    export default defineComponent({
+        name: 'Notificacoes',
+    })
+</script>
+
+<style scoped>
+.notificacoes {
+    right: 15px;
+    width: 300px;
+    padding: 8px;
+    z-index: 105;
+    position: absolute;
+}
+</style>
