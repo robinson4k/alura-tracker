c0eb0769b208f26f4b83c0f488918e2b83746240
Robinson Silva <robinson.silva@grupohpar.com>
2023-03-17 17:40:04 -0400

    Reagindo a mudanças

--- src/views/Tarefas.vue
+++ src/views/Tarefas.vue
@@ -44,7 +44,7 @@
 </template>
 
 <script lang="ts">
-import { computed, defineComponent, ref } from 'vue';
+import { computed, defineComponent, ref, watchEffect } from 'vue';
 import Formulario from '../components/Formulario.vue';
 import Tarefa from '../components/Tarefa.vue';
 import Box from '../components/Box.vue';
@@ -91,14 +91,12 @@ export default defineComponent({
 
         const filtro = ref("")
 
-        const tarefas = computed(() => 
-            store.state.tarefas.filter(
-                (t) => !filtro.value || t.descricao.includes(filtro.value)
-            )
-        )
+        watchEffect(() => {
+            store.dispatch(OBTER_TAREFAS, filtro.value)
+        })
 
         return {
-            tarefas,
+            tarefas: computed(() => store.state.tarefas),
             store,
             filtro
         }
