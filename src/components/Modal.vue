dda4dd7008382afca8f9f0206853c12ebae10f04
Robinson Silva <robinson.silva@grupohpar.com>
2023-03-27 00:44:29 -0400

    Componentizando a modal

---
+++ src/components/Modal.vue
@@ -0,0 +1,29 @@
+<template>
+    <div class="modal" :class="{ 'is-active' : mostrar }" v-if="mostrar">
+        <div class="modal-background"></div>
+        <div class="modal-card">
+            <slot />
+        </div>
+    </div>
+</template>
+
+<script lang="ts">
+import { computed, defineComponent, ref, watchEffect } from 'vue';
+import Formulario from '../components/Formulario.vue';
+import Tarefa from '../components/Tarefa.vue';
+import Box from '../components/Box.vue';
+import { useStore } from '@/store';
+import { ALTERAR_TAREFA, CADASTRAR_TAREFA, OBTER_PROJETOS, OBTER_TAREFAS } from '@/store/tipo-acoes';
+import ITarefa from '@/interfaces/ITarefa';
+
+export default defineComponent({
+    name: "Modal",
+    props: {
+        mostrar: {
+            type: Boolean,
+            required: true
+        }
+    },
+
+})
+</script>
--- src/views/Tarefas.vue
+++ src/views/Tarefas.vue
@@ -16,30 +16,27 @@
 
         <Tarefa v-for="(tarefa, index) in tarefas" :key="index" :tarefa="tarefa" @aoTarefaClicada="selecionarTarefa" />
 
-        <div class="modal" :class="{ 'is-active' : tarefaSelecionada }" v-if="tarefaSelecionada">
-            <div class="modal-background"></div>
-            <div class="modal-card">
-                <header class="modal-card-head">
-                    <p class="modal-card-title">Editando uma tarefa</p>
-                    <button class="delete" aria-label="close" @click="fecharModal"></button>
-                </header>
-                <section class="modal-card-body">
-                    <div class="field">
-                        <label for="descricaoDaTarefa" class="label">Descrição</label>
-                        <input
-                            type="text"
-                            class="input"
-                            v-model="tarefaSelecionada.descricao"
-                            id="descricaoDaTarefa"
-                        />
-                    </div>
-                </section>
-                <footer class="modal-card-foot">
-                    <button @click="alterarTarefa" class="button is-success">Salvar</button>
-                    <button @click="fecharModal" class="button">Cancelar</button>
-                </footer>
-            </div>
-        </div>
+        <Modal :mostrar="tarefaSelecionada != null">
+            <header class="modal-card-head">
+                <p class="modal-card-title">Editando uma tarefa</p>
+                <button class="delete" aria-label="close" @click="fecharModal"></button>
+            </header>
+            <section class="modal-card-body">
+                <div class="field">
+                    <label for="descricaoDaTarefa" class="label">Descrição</label>
+                    <input
+                        type="text"
+                        class="input"
+                        v-model="tarefaSelecionada.descricao"
+                        id="descricaoDaTarefa"
+                    />
+                </div>
+            </section>
+            <footer class="modal-card-foot">
+                <button @click="alterarTarefa" class="button is-success">Salvar</button>
+                <button @click="fecharModal" class="button">Cancelar</button>
+            </footer>
+        </Modal>
     </div>
 </template>
 
@@ -47,6 +44,7 @@
 import { computed, defineComponent, ref, watchEffect } from 'vue';
 import Formulario from '../components/Formulario.vue';
 import Tarefa from '../components/Tarefa.vue';
+import Modal from '../components/Modal.vue';
 import Box from '../components/Box.vue';
 import { useStore } from '@/store';
 import { ALTERAR_TAREFA, CADASTRAR_TAREFA, OBTER_PROJETOS, OBTER_TAREFAS } from '@/store/tipo-acoes';
@@ -57,6 +55,7 @@ export default defineComponent({
     components: {
         Formulario,
         Tarefa,
+        Modal,
         Box
     },
     data () {
